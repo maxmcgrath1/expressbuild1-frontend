26edd62fa9d3b3f2f6cf6d05736004c5c5ae7803
maxmcgrath1 <tuc06096@temple.edu>
2022-03-03 11:36:14 -0500

    updated people.js

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import {Link} from "react-router-dom"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 function People(props) {
     // state to hold formData
@@ -7,38 +7,40 @@ function People(props) {
         name: "",
         image: "",
         title: "",
-    });
+    })
 
     // handleChange function for form
-    const handleChange = (event) => {
-        setNewForm({ ...newForm, [event.target.name]: event.target.value });
-    };
+    const handleChange = event => {
+        setNewForm({ ...newForm, [event.target.name]: event.target.value })
+    }
 
     // handle submit function for form
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        props.createPeople(newForm);
+    const handleSubmit = event => {
+        event.preventDefault()
+        props.createPeople(newForm)
         setNewForm({
             name: "",
             image: "",
             title: "",
-        });
-    };
+        })
+    }
 
     // loaded function
     const loaded = () => {
-        return props.people.map((person) => (
+        return props.people.map(person => (
             <div key={person._id} className="person">
-                <Link to={`/people/${person._id}`}><h1>{person.name}</h1></Link>
+                <Link to={`/people/${person._id}`}>
+                    <h1>{person.name}</h1>
+                </Link>
                 <img src={person.image} alt={person.name} />
                 <h3>{person.title}</h3>
             </div>
-        ));
-    };
+        ))
+    }
 
     const loading = () => {
-        return <h1>Loading...</h1>;
-    };
+        return <h1>Loading...</h1>
+    }
     return (
         <section>
             <form onSubmit={handleSubmit}>
@@ -67,7 +69,7 @@ function People(props) {
             </form>
             {props.people ? loaded() : loading()}
         </section>
-    );
+    )
 }
 
-export default People;
+export default People
